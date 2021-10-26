a153d91a09f6436f8a04088fbddee6f1f1e18e50
Mike Carlson <21209421+redfoxseattle@users.noreply.github.com>
2021-10-26 12:02:10 -0700

    added solutions for day 5 and reorganized files

---
+++ Exercises/04 - Array Cardio Day 1/arrayCardio.test.js
@@ -0,0 +1,44 @@
+import * as AC from "./arrayCardio.js";
+
+
+console.log(AC.bornFirst()) 
+// console.log(fullNames) 
+// console.log(birthdaySort) 
+// console.log(findYears) 
+// console.log(yearsSort) 
+// console.log(containsWord)
+  
+sortByLastName() 
+
+sumItems()
+
+//console.log(arrayCardio)
+console.log(sumItems)
+// function wait(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, ms)
+//   })
+// }
+
+
+
+// test('async support', async () => {
+//   await wait(10)
+
+//   expect(true).toBe(true)
+// })
+
+// test('ES6 support', () => {
+//   expect(fancyEs6Stuff(['a', 'b', 'c'], [1, 2, 3])).toEqual(['a', 'b', 'c', 1, 2, 3])
+// })
+
+// describe('My Test Suite', () => {
+//   it('My Test Case', () => {
+//     expect(sumItems()).toEqual(true)
+//   })
+// })
+// describe('My work', () => {
+//   test('works', () => {
+//     expect(2).toEqual(2)
+//   })
+// })
