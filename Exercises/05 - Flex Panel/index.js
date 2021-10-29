b8662e9b4f2c40d6842819e95e0cb0205863ebc0
Mike Carlson <21209421+redfoxseattle@users.noreply.github.com>
2021-10-28 23:58:16 -0700

    added Flex Panel solution

---
+++ Exercises/05 - Flex Panel/index.js
@@ -0,0 +1,16 @@
+/* const panels = document.querySelectorAll('.panel')
+console.log(panels)
+
+function toggleOn() {
+    this.classList.toggle('open')
+}
+
+function toggleAction(event) {
+    console.log(this.classList)
+    if (event.propertyName.includes('flex')) {
+        this.classList.toggle('active')
+    }
+}
+
+panels.forEach((panel) => panel.addEventListener('mouseup', toggleOn))
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleAction)) */
