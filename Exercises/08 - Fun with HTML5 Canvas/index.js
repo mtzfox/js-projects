33377834fac4891847f0e6f6c03a12b441d42e5a
Mike Carlson <21209421+redfoxseattle@users.noreply.github.com>
2021-11-14 23:15:02 -0800

    day 8 files

---
+++ Exercises/08 - Fun with HTML5 Canvas/index.js
@@ -0,0 +1,8 @@
+// // select canvas
+// const canvas = document.querySelector('#draw');
+
+// // Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
+// const ctx = canvas.getContext('2d');
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
